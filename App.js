069d361e39343599fbb5f2fcc4e1ee36a6baf7ef
Md-Saif-Ali <119032699+Md-Saif-Ali@users.noreply.github.com>
2022-11-26 12:00:55 +0530

    Update App.js

--- App.js
+++ App.js
@@ -1,1 +1,17 @@
+const x=document.getElementById('image');
 
+function home() {
+	x.src="https://storiespub.com/wp-content/uploads/2022/08/8-819x1024.webp";
+}
+
+function work() {
+	x.src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTcB1SsrzumPMrULUvDt1pe71ds0Uh0NRHGCg&usqp=CAU";
+}
+
+function blog() {
+	x.src="https://revenuearchitects.com/wp-content/uploads/2017/02/Blog_pic.png";
+}
+
+function aboutUs() {
+	x.src="https://blog.advesa.com/wp-content/uploads/2019/11/about-us-page.png";
+}
